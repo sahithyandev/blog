8b1e047d21fa33b37f26dfcf00bbb3eaf93e421f
Sahithyan <sahithyan2701@gmail.com>
2021-04-30 09:49:07 +0530

    ..

--- pages/about.jsx
+++ pages/about.jsx
@@ -6,8 +6,10 @@ import { SITE_CONSTANTS } from "../global"
 
 import styles from "@/styles/about.module.css"
 import postStyles from "@/styles/blog-post.module.css"
+import { CustomLink } from "@/components/MDXComponents"
 
 const AboutPage = () => {
+
 	return (
 		<>
 			<HeadBase
@@ -29,19 +31,26 @@ const AboutPage = () => {
 				<h1>I am</h1>
 
 				<p>
-					Hello!, I am Sahithyan, a 17-year-old web developer, a student and a tech enthusiast. I am from Jaffna, Sri Lanka. I like to code in JavaScript and TypeScript. And as a front end developer, I also know HTML & CSS. I learnt Python and Go too (But I don't use them currently).
+					Hello!, I am Sahithyan, a 17-year-old web developer, a student and a tech enthusiast. I am from Jaffna, Sri Lanka.
+				</p>
+
+				<p>
+					I know HTML, CSS, JavaScript, TypeScript and some Python.
 				</p>
 
 				<h3>About the blog</h3>
 				<p>
-					This is a place for me to write on the things I love: Web Dev, Tech and Mathematics.
+					<strong>This is a place for me to write on the things I love: Web Dev, Tech and Mathematics.</strong><br /><br />
+
+					This blog is built using <CustomLink href="https://nextjs.org">Next.js</CustomLink> and <CustomLink href="https://mdx.js">MDX</CustomLink>. To learn more, read <CustomLink href="/post/introduction-to-my-blog">Introduction To My Blog</CustomLink>.
 				</p>
+
 
 				{/* TODO add my skills section */}
 
 				<h3>Contact me</h3>
 				<p>
-					The fastest and the easiest way would be direct messaging me on Twitter <a href="https://www.twitter.com/iamSahithyan">@iamSahithyan</a>. You can also email me at <a className="formatted" href="mailto:sahithyan2701@gmail.com">sahithyan2701@gmail.com</a>
+					The fastest and the easiest way would be direct messaging me on Twitter <CustomLink href="https://www.twitter.com/iamSahithyan">@iamSahithyan</CustomLink>. You can also use the below-listed social media accounts too. And you can also email me at <CustomLink href="mailto:sahithyan2701@gmail.com">sahithyan2701@gmail.com</CustomLink>.
 				</p>
 
 				<div style={{ margin: 'auto', width: "fit-content" }}>
