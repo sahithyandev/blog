cfb1778249334d7c5b8b59930c3269aa401c172b
Sahithyan <sahithyan2701@gmail.com>
2021-07-06 20:21:56 +0530

    some more configuration

--- components/GlobalStyles.jsx
+++ components/GlobalStyles.jsx
@@ -3,6 +3,21 @@ import { createGlobalStyle } from "styled-components";
 import tw, { theme, GlobalStyles as BaseStyles } from "twin.macro";
 
 const CustomStyles = createGlobalStyle`
+	:root {
+		font-size: 18px;
+		color-scheme: dark;
+		
+		--dark: #131315;
+		--light: #FFFBFE;
+		--main: #FAF33E;
+		--main-half-opacity: #faf33e70;
+		--secondary: #087F8C;
+		--post-card-bg: hsl(236, 10%, 25%);
+		--post-card-bg-hover: hsl(220, 12%, 35%);
+		
+		--social-twitter: hsl(203, 89%, 42%);
+	}
+
 	* {
 		box-sizing: border-box;
 	}
@@ -17,12 +32,19 @@ const CustomStyles = createGlobalStyle`
 		color: var(--light);
 		scroll-behavior: smooth;
 	}
+
+	@media (max-width: 600px) {
+		html, body {
+			max-width: 90vw;
+			width: auto; 
+		}
+	}	
 `;
 
 export const GlobalStyles = () => {
 	return (
 		<>
-			<BaseStyles />
+			{/* <BaseStyles /> */}
 			<CustomStyles />
 		</>
 	);
---
+++ tailwind-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	darkMode: false,
+	theme: {
+		colors: {
+			dark: "#131315",
+			light: "#FFFBFE",
+			main: "#FAF33E",
+			secondary: "#087F8C"
+		}
+	}
+}
