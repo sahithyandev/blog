0240300889b03b0659b5dd2de79673dc1aa3e5ad
Sahithyan <sahithyan2701@gmail.com>
2021-07-07 21:21:33 +0530

    tailwind config fixed

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+	darkMode: false,
+	theme: {
+		colors: {
+			dark: "#131315",
+			light: "#FFFBFE",
+			main: "#FAF33E",
+			secondary: "#087F8C"
+		},
+		fontSize: {
+			'xs': '.75rem',
+			'sm': '.875rem',
+			'small': '.9em',
+			'base': '1rem',
+			'lg': '1.125rem',
+			'xl': '1.25rem',
+			'2xl': '1.5rem',
+			'3xl': '1.875rem',
+			'4xl': '2.25rem',
+			'5xl': '3rem',
+			'6xl': '4rem',
+			'7xl': '4.5rem',
+			'8xl': '6rem',
+			'9xl': '8rem'
+		}
+	}
+}
