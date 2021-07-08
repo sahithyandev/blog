167fe9d79080f484d0be95b3b411a6da3bd2ddc4
Sahithyan <sahithyan2701@gmail.com>
2021-07-08 10:20:33 +0530

    tailwind migration: page/index

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,7 @@
-import Head from "next/head"
+import tw from "twin.macro";
 
 import { HeadBase, Nav, Footer, PostCard, SocialLinks } from '@/components'
 import { getAllPosts } from "@/helpers/post"
-
-import styles from '@/styles/home.module.css'
 
 export default function Home({ latestPosts }) {
   return (
@@ -13,16 +11,16 @@ export default function Home({ latestPosts }) {
       <Nav hideLogo={true} />
 
       <main>
-        <div className={styles["me-banner"]}>
-          <h1 className={styles["banner--name"]}>Sahithyan</h1>
-          <div className={styles["small-intro"]}>student, web developer & tech enthusiast</div>
+        <div css={tw`flex flex-col items-center mt-12 mb-16`}>
+          <h1 css={tw`font-bold mb-2 text-3xl`}>Sahithyan</h1>
+          <div css={tw`opacity-80 text-lg`}>student, web developer & tech enthusiast</div>
 
           <SocialLinks />
         </div>
 
 
         <section>
-          <h2 className={styles["latest-posts-heading"]}>Latest Posts</h2>
+          <h2 css={tw`text-xl`}>Latest Posts</h2>
 
           <div className="posts-container">
             {latestPosts.map(post => <PostCard key={post.meta.title} {...post.meta} />)}
