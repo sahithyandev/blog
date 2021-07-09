6bd130743c57eb362a1d55040d54c8aa9f66944c
Sahithyan <sahithyan2701@gmail.com>
2021-07-09 22:14:12 +0530

    tailwind migration: component/mdx/blockquote

--- components/MDX/BlockQuote.jsx
+++ components/MDX/BlockQuote.jsx
@@ -1,13 +1,23 @@
+import tw, { css } from "twin.macro";
+
 const BlockQuote = ({
 	children: quote,
 	citationLink = "",
 	citationText = ""
 }) => {
-	return <figure>
-		<blockquote cite={citationLink}>
+	return <figure css={tw`ml-0 max-w-prose`}>
+		<blockquote
+			cite={citationLink}
+			css={tw`ml-4`}
+		>
 			{quote}
 		</blockquote>
-		<figcaption>
+		<figcaption css={css`
+			:before {
+				content: "— ";
+			}
+			${tw`text-right`}
+		`}>
 			<cite>{citationText}</cite>
 		</figcaption>
 	</figure>
